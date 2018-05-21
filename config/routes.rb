19ab6426d175180ed5ad7c6427bc55fdb79bf076
AndyCD <andreea.c.damian@gmail.com>
2018-05-21 10:02:28 -0400

    Fixed Dropdown in Navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'main#home'
+
+  get 'action', to: 'main#action'
+  get 'another_action', to: 'main#another_action'
+  get 'another_another_action', to: 'main#another_another_action'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
